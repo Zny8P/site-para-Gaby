b2e0a66661a525b18d2b7f72e948fb0c8f0fc6ea
Zny_8P <fabriciomarcosv@gmail.com>
2026-08-08 13:33:58 -0300

    button movement with a parable

--- script.js
+++ script.js
@@ -1,33 +1,67 @@
 const buttonContainer0 = document.getElementById("buttonContainer0");
 const button0 = document.getElementById("button0");
+let indoParaNoite = false
 
+let inicio = null;
+function animarSol_lua(tempo){
+    if(inicio === null) inicio = tempo;
+
+    let progress = (tempo - inicio) / 1000;
+    if (progress > 1) progress = 1;
+
+    let x = progress
+    let y = -4*x*x + 4*x 
+
+    let distanciax = window.innerWidth * 0.83;
+    let alturaArco = window.innerHeight * 0.30;
+
+    let deslocamentox = 0;
+    let deslocamentoy = 0;
+
+    if (indoParaNoite){        
+        distanciax =  window.innerWidth * 0.83;
+        alturaArco = window.innerHeight * 0.30;
+
+        deslocamentox = distanciax * x
+        deslocamentoy = -alturaArco * y    
+    }else{        
+        deslocamentox = -distanciax * x
+        deslocamentoy = alturaArco * y    
+    };
+
+    buttonContainer0.style.transform =
+    `translate(${deslocamentox}px, ${deslocamentoy}px)`;
+
+    if(progress < 1){
+        requestAnimationFrame(animarSol_lua);
+    }else{
+        inicio = null
+
+        if(indoParaNoite){
+            button0.classList.remove("day");
+            buttonContainer0.classList.remove("day");
+            button0.classList.add("night");
+            buttonContainer0.classList.add("night");
+        }else{
+            button0.classList.remove("night");
+            buttonContainer0.classList.remove("night");
+            button0.classList.add("day");
+            buttonContainer0.classList.add("day");
+        }
+
+        buttonContainer0.style.transform = "translate(0px, 0px)";
+    }
+}
 
 button0.onclick = function() {
-    button0.classList.toggle("noite");
-    button0.classList.toggle("dia");
 
-    if(button0.classList.contains("noite")){
-        buttonContainer0.classList.add("sol_lua");
+    indoParaNoite = button0.classList.contains("day")
+    // alert(indoParaNoite)
+
+    if(indoParaNoite){
         button0.innerHTML = "🌙";
     }else{
         button0.innerHTML = "☀️";
-        buttonContainer0.classList.add("lua_sol");
     }
+    requestAnimationFrame(animarSol_lua);
 };
-buttonContainer0.addEventListener("animationend", function() {
-    buttonContainer0.classList.remove("sol_lua");
-    buttonContainer0.classList.remove("lua_sol");
-
-    buttonContainer0.classList.toggle("dia");
-    buttonContainer0.classList.toggle("noite");
-
-
-    if(button0.classList.contains("noite")){
-        buttonContainer0.classList.add("noite");
-        buttonContainer0.classList.remove("dia");
-    }else{
-        buttonContainer0.classList.add("dia");
-        buttonContainer0.classList.remove("noite");
-    }
-
-});
